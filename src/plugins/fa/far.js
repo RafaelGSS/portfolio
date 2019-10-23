ee42adf58dd1f1b07aa4596a61f4810fd7e7b19a
RafaelGSS <rafael.nunu@hotmail.com>
2019-10-22 23:47:22 -0300

    chore: update github ci

--- src/plugins/fa/far.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import {} from '@fortawesome/free-regular-svg-icons'
-
-library.add()
--- src/plugins/fa/index.js
+++ src/plugins/fa/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import './fab'
-import './far'
 import './fas'
 
 Vue.component('fa', FontAwesomeIcon)
